4ed6a3d52c8531998b6643b07edf1456d609290d
souhila boukari <suhamimisou14@gmail.com>
2022-01-03 14:37:25 -0500

    ask the user type of the characters for a password

--- Develop/script.js
+++ Develop/script.js
@@ -31,3 +31,37 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
     // ask the user if they want special char
     var doTheyWantSpecialCharacter = window.confirm("do you want Special character?(ok for yes, cancel for no)  ");
     console.log(doTheyWantSpecialCharacter);
+    // if they want a number
+  if(doTheyWantNumbers === true){
+    availChar = availChar + num;
+  };
+  // -- add 0-9 to that avail char array
+  // -- ex: availChar = availChar + '0123456789'
+  // if they want a spec char
+  // -- add spec char
+  if(doTheyWantSpecialCharacter === true){
+    availChar = availChar + special;
+  };
+  // -- if both chose then available char === '123456789!@#$%^&*'
+  // if they want a lowercase letter
+  if(doTheyWantLowercase === true){
+    availChar = availChar + lowerCase;
+  };
+  // -- add some lowercase letters
+  // if they want uppercase
+  if(doTheyWantUppercase=== true){
+    availChar = availChar + upperCase;
+  };
+  // -- add some uppercase letters
+
+  // for each char in the passwordLength
+  var passwordString = '';
+  for ( var i = 0; i < passwordLength; i++ ){
+    passwordString += availChar[Math.floor(Math.random() * (availChar.length))];
+  };
+  // -- randomly select from our list of avail char
+  
+
+  return passwordString;
+
+}
